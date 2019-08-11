3798b94f7f2d6e51b9ae5ec25536e3684b813716
lengyefenghan <lengyefenghan@outlook.com>
2019-08-11 18:03:08 +0800

    fix js

---
+++ js/about.js
@@ -0,0 +1,12 @@
+// JavaScript source code
+document.writeln("<a href=../blog/2018-02-21.html\>360N4S骁龙版</a>");
+document.writeln("<p>昵称:&emsp;冷夜枫寒&emsp;&#124;&emsp;Lengyefenghan</p>");
+document.writeln("<p>职业:&emsp;无业游民</p>");
+document.writeln("<p>地址:&emsp;河南-开封</p>");
+document.writeln("<p>邮箱:&emsp;lengyefenghan@dingtalk.com</p>");
+document.writeln("<p>QQ:&emsp;905974035</p>");
+//<p>昵称:&emsp;冷夜枫寒&emsp;&#124;&emsp;Lengyefenghan</p>
+//<p>职业:&emsp;无业游民</p>
+//<p>地址:&emsp;河南-开封</p>
+//<p>邮箱:&emsp;lengyefenghan@dingtalk.com</p>
+//<p>QQ:&emsp;905974035</p>
