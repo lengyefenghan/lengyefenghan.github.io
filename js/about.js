fc57250f44c74899fd5b8b4639cfe6c5befaf342
lengyefenghan <lengyefenghan@outlook.com>
2020-01-02 00:52:16 -0800

    test

--- js/about.js
+++ js/about.js
@@ -1,9 +1,8 @@
 // JavaScript source code
-document.writeln("<a href=../blog/2018-02-21.html\>360N4S骁龙版</a>");
-document.writeln("<p>昵称:&emsp;冷夜枫寒&emsp;&#124;&emsp;Lengyefenghan</p>");
+document.writeln("<p>昵称:&emsp;冷夜枫寒</p>");
 document.writeln("<p>职业:&emsp;无业游民</p>");
 document.writeln("<p>地址:&emsp;河南-开封</p>");
-document.writeln("<p>邮箱:&emsp;lengyefenghan@dingtalk.com</p>");
+//document.writeln("<p>邮箱:&emsp;</p>");
 document.writeln("<p>QQ:&emsp;905974035</p>");
 //<p>昵称:&emsp;冷夜枫寒&emsp;&#124;&emsp;Lengyefenghan</p>
 //<p>职业:&emsp;无业游民</p>
