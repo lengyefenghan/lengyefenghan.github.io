c38c4294443caa598ab5c13e18cb7505ab82c783
firewall <30388496+lengyefenghan@users.noreply.github.com>
2020-01-15 13:12:40 +0800

    add gonganbeian

--- js/footer.js
+++ js/footer.js
@@ -1,1 +1,5 @@
-document.writeln("<a href=\" " + "http://www.beian.miit.gov.cn/" + "\" " + ">" + "<p>豫ICP备19043668号</p>" + "</a>");
+document.writeln("<a href=\"http://www.beian.miit.gov.cn/\"><p>豫ICP备19043668号</p></a>");
+document.writeln("<div style=\"width:300px;margin:0 auto; padding:20px 0;\">")
+document.writeln("<a target=\"_blank\" href=\"http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=41022202000034\" style=\"display:inline-block;text-decoration:none;height:20px;line-height:20px;\"><img src=\"\" style=\"float:left;\"/><p style=\"float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px; color:#939393;\">豫公网安备 41022202000034号</p></a>")
+document.writeln("</div>")
+		 
